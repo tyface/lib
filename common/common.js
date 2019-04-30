cecbf5e56826a2013fdad7d1db7d5241ee1740fb
tyface <zxuz34@gmail.com>
2019-04-30 14:00:04 +0900

    Update common.js

--- common/common.js
+++ common/common.js
@@ -1,9 +1,23 @@
-/* 엔터키 이벤트시 함수실행(모바일 디바이스 포함) */
-//사용법: ex) input 태그안에 onkeypress="chkEnter(event, 실행할 함수, [함수에 필요한 아규먼트/없어도 무방])"
+/* 
+엔터키 이벤트시 함수실행(모바일 디바이스 포함)
+사용법: ex) input 태그안에 onkeypress="chkEnter(event, 실행할 함수, [함수에 필요한 아규먼트/없어도 무방]);"
+*/
 function chkEnter(evt, fnc, args) {
 	var keyCode = evt.which ? evt.which : event.keyCode;
 
 	if(keyCode == "13") { //키코드 13 : enter
 		fnc(args);
+	}
+}
+
+/* 
+엔터키 이벤트시 포커스 아웃(모바일 키패드용)
+사용법: ex) input 태그에 onkeypress="focusOut(event);"
+*/
+function FocusOutByEnter(evt) {
+	var keyCode = evt.which?evt.which:event.keyCode;
+
+	if(keyCode == "13") {
+		document.activeElement.blur();
 	}
 }
